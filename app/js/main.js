98d40e0548742b4f48d6ca27b12595a1001c9e8f
Evandro <evandro@localhost.localdomain>
2017-09-26 17:29:00 -0400

    Organização dos campos de entrada -- Adicionado loader ao concluir operação

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,16 @@
 console.log('Edite o arquivo js/main.js.')
+
+var submit = false;
+var form = document.getElementById("form")
+form.onsubmit = (e) => {
+  e.preventDefault()
+  showLoader(form);
+}
+
+var showLoader = (form) =>{
+	document.getElementsByClassName('loader')[0].style.visibility = 'visible'
+	setTimeout(() => {
+  document.getElementsByClassName('loader')[0].style.visibility = 'hidden'
+  form.submit();
+},2000)
+}
